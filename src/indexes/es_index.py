f89f1861eaa003ab6db47bbb2bc8f7c56051e60e
Safir <safir.najafi@gmail.com>
2017-05-04 01:35:10 +0200

    Implemented insert_document

--- src/indexes/es_index.py
+++ src/indexes/es_index.py
@@ -1,15 +1,28 @@
-from .base_index import BaseIndex
+from base_index import BaseIndex
+import json
+import httplib
 
+URL = "localhost:9200"
+INDEX_PATH = "images/image/"
 
-class ESIndex(BaseIndex):
-    # TODO: @Safir, your implementation goes here
-    def insert_document(self, document_dict):
-        pass
+class ESIndex():
+	def __init__(self):
+		self.counter = 1
 
-    def query_index(self, query_dict, query_fields):
-        pass
+	def insert_document(self, document_dict):
+		with open(document_dict, 'r') as file:
+			data = file.read()
+		self.connection = httplib.HTTPConnection(URL)
+		self.connection.request('POST', INDEX_PATH + str(self.counter), data, { "Content-Type": "application/json" })
+		response = json.loads(self.connection.getresponse().read().decode())
+		self.counter += 1
+		self.connection.close()
+		return response
 
-    def persist_index(self):
-        pass
+	# On it, just need to read up on docs
+	def query_index(self, query_dict, query_fields):
+		pass
 
-
+	# On hold
+	def persist_index(self):
+		pass
